77b20ad6a189f83424f3794896a35e282551e33f
Arnau Besora <abesorasans@gmail.com>
2020-12-18 18:10:34 +0100

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dominos.MixProject do
     [
       app: :dominos,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
